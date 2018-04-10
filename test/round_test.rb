f32e88c238ff263ed35700a5125c0d6abfed0c14
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-04-09 20:12:20 -0600

    Began iteration 4, only initial tests pass

---
+++ test/round_test.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/guess'
+require './lib/card'
+require './lib/deck'
+require './lib/round'
+require 'pry'
+
+class RoundTest < Minitest::Test
+
+  def test_it_exists
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau")
+    card_2 = Card.new("Approximately how many miles are in one astronomical unit?", "93,000,000")
+    deck = Deck.new([card_1, card_2])
+    round = Round.new(deck)
+    assert_instance_of Round, round
+    assert_instance_of Deck, round.deck
+  end
+
+
+end
