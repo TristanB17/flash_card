f2abd9545045428730778a7b029843b2af29d4f9
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-04-10 19:59:12 -0600

    Created runner file

---
+++ lib/flashcard_runner.rb
@@ -0,0 +1,9 @@
+require './lib/guess'
+require './lib/card'
+require './lib/deck'
+require './lib/round'
+
+card_1 = Card.new("What is the capital of Alaska?", "Juneau")
+card_2 = Card.new("Approximately how many miles are in one astronomical unit?", "93,000,000")
+deck = Deck.new([card_1, card_2])
+round = Round.new(deck)
